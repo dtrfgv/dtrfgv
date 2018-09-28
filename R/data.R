7dcdf84044db904f5c77d54411a39d84279f7c1c
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2018-09-28 13:20:01 +0200

    merge all datasets doc in data.R file

---
+++ R/data.R
@@ -0,0 +1,109 @@
+#' Data to test function rfgv
+#'
+#' A dataset generated to check that fucntion rfgv works.
+#' Please load with rfgv_dataset
+#'
+#' 26 values 
+"group"
+
+#' Data for decision tree tutorial
+#'
+#' A dataset from Lise Vaudor blog post about decision tree
+#'
+#' @format A data frame with 78 rows and 5 variables:
+#' \describe{
+#'   \item{Prix}{ Item prices}
+#'   \item{Flashitude}{  degree of beauty}
+#'   \item{Branchitude}{ degree of Hype}
+#'   \item{Qualite}{ degree of robustness}
+#'   \item{Achat}{ number of sells}
+#' }
+"magasin"
+
+#' Data to test function rfgv
+#'
+#' A dataset generated to check that fucntion rfgv works.
+#'
+#' @format A data frame with 1000 rows and 27 variables:
+#' \describe{
+#'   \item{\code{Type}}{train, test, validation}
+#'   \item{\code{Y}}{0 or 1}
+#'   \item{\code{V3_G1}}{ some values }
+#'   \item{\code{V4_G1}}{ some values }
+#'   \item{\code{V5_G1}}{ some values }
+#'   \item{\code{V6_G1}}{ some values }
+#'   \item{\code{V7_G1}}{ some values }
+#'   \item{\code{V8_G2}}{ some values }
+#'   \item{\code{V9_G2}}{ some values }
+#'   \item{\code{V10_G2}}{ some values }
+#'   \item{\code{V11_G2}}{ some values }
+#'   \item{\code{V12_G2}}{ some values }
+#'   \item{\code{V13_G3}}{ some values }
+#'   \item{\code{V14_G3}}{ some values }
+#'   \item{\code{V15_G3}}{ some values }
+#'   \item{\code{V16_G3}}{ some values }
+#'   \item{\code{V17_G3}}{ some values }
+#'   \item{\code{V18_G4}}{ some values }
+#'   \item{\code{V19_G4}}{ some values }
+#'   \item{\code{V20_G4}}{ some values }
+#'   \item{\code{V21_G4}}{ some values }
+#'   \item{\code{V22_G4}}{ some values }
+#'   \item{\code{V23_G5}}{ some values }
+#'   \item{\code{V24_G5}}{ some values }
+#'   \item{\code{V25_G5}}{ some values }
+#'   \item{\code{V26_G5}}{ some values }
+#'   \item{\code{V27_G5}}{ some values }
+#' }
+
+
+"rfgv_dataset"
+#' Species to predict
+#'
+#' A dataset from Lise Vaudor blog post about random forest
+#' It is link with iris dataset
+#'
+#' @format A data frame with 50 rows and 4 variables:
+#' \describe{
+#'   \item{Sepal.Length}{ Sepal length}
+#'   \item{Sepal.Width}{  Sepal width}
+#'   \item{Petal.Length}{ Petal length}
+#'   \item{Petal.Width}{ Petal width}
+#' }
+
+
+"Species_to_predict"
+#' Data to test function predict_cartgv
+#'
+#' A dataset generated to check that fucntion predict_cartgv works.
+#'
+#' @format A list of data frames
+"carts"
+
+#' Dataframe contains in data_phoneme
+#' @format data frame
+"coups"
+
+#' Dataframe contains in data_phoneme
+#' @format data frame
+"new"
+
+#' Dataframe contains in data_phoneme
+#' @format data frame
+"test"
+
+#' Dataframe contains in data_phoneme
+#' @format data frame
+"train"
+
+#' Dataframe contains in data_phoneme
+#' @format data frame
+"tree"
+
+#' Dataframe contains in data_phoneme
+#' @format data frame
+"tree.cartgv"
+
+#' Dataframe contains in data_phoneme
+#' @format data frame
+"group"
+
